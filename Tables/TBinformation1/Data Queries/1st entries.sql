86f52a9b518a844188db0ed94215313bf74a145b
acorns15 <81124657+acorns15@users.noreply.github.com>
2021-04-01 12:24:31 +0800

    Create 1st entries.sql

---
+++ Tables/TBinformation1/Data Queries/1st entries.sql
@@ -0,0 +1,60 @@
+INSERT INTO TBinformation1 (
+        TBinformation1,
+        FullName,
+        Birthday,
+        IsActive,
+        Allowance
+        )
+    VALUES (
+        '1',
+        'Jess Dales Dela Cruz Sr.',
+        '1982-07-30',
+        '1',
+        '100'
+    );
+
+INSERT INTO TBinformation1 (
+        TBinformation1,
+        FullName,
+        Birthday,
+        IsActive,
+        Allowance
+        )
+    VALUES (
+        '2',
+        'Rupert Dale Dela Cruz',
+        '2000-12-01',
+        '1',
+        '100'
+    );
+
+INSERT INTO TBinformation1 (
+        TBinformation1,
+        FullName,
+        Birthday,
+        IsActive,
+        Allowance
+        )
+    VALUES (
+        '1',
+        'Jaden Chalaire Dela Cruz',
+        '2002-09-10',
+        '1',
+        '100'
+    );
+
+INSERT INTO TBinformation1 (
+        TBinformation1,
+        FullName,
+        Birthday,
+        IsActive,
+        Allowance
+        )
+    VALUES (
+        '1',
+        'Seven Dale Dela Cruz',
+        '2003-10-14',
+        '1',
+        '100'
+    );
+
