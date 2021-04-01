ecac036ed4663c72ecc6db8d4981064ebb1a3c6e
acorns15 <81124657+acorns15@users.noreply.github.com>
2021-04-01 20:00:49 +0800

    Update

--- Tables/TBinformation1/Data Queries/Sample Updates.sql
+++ Tables/TBinformation1/Data Queries/Sample Updates.sql
@@ -1,6 +1,7 @@
 UPDATE TBinformation1 /*Update function is to edit the existing data*/
-    SET FullName = 'Jess Dales Dela Cruz Sr.'
+    SET FullName = 'Jess Dales Dela Cruz'
     /*SET where in the existing record located*/
-    WHERE FullName = 'Jess Dales Dela Cruz';
+    WHERE FullName = 'Jess Dales Dela Cruz Sr.';
     /* WHere is the function that which record that should be updated
         if ommited all records in the table will be updated */
+GO
