0a81e06fe8ddab0c4b74886d2103aaaa89e6d9e4
Maycort Baculna <81124657+acorns15@users.noreply.github.com>
2021-04-05 07:58:40 +0800

    Update Creating Table1.sql

--- Tables/Table1/Creating Table1.sql
+++ Tables/Table1/Creating Table1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Table1 (
-    CustID1 INT NOT NULL IDENTITY (1,1),
+    CustID1 INT NOT NULL /*****IDENTITY (1,1)*****//*IDENTITY (Initial Value, Increment Value)*//*****IDENTITY function is to automate the interger values without inserting values. It generates values based on initial value and step(increment) value *****/,
     Name VARCHAR(100),
     City VARCHAR(100),
     Type CHAR (10),
-    CONSTRAINT PK_Table1 PRIMARY KEY (CustID1)
+    CONSTRAINT PK_Table1 /**Name of the Primary Key**/ PRIMARY KEY (CustID1)
 );
