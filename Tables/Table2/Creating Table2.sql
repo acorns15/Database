eb21991ff2a497ab42a45dbf5a39a78498807ef6
Maycort Baculna <81124657+acorns15@users.noreply.github.com>
2021-04-05 08:04:58 +0800

    Update Creating Table2.sql

--- Tables/Table2/Creating Table2.sql
+++ Tables/Table2/Creating Table2.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Table2 (
-    CustID2 INT NOT NULL IDENTITY (1,1),
+    CustID2 INT NOT NULL /*****IDENTITY (1,1)*****//****See Note in table1***/,
     Monthly_Sales Money,
     Sales_Discount Money,
     CONSTRAINT PK_Table2 PRIMARY KEY (CustID2),
-    CONSTRAINT FK_Table2_CustID1 FOREIGN KEY (CustID2)
+    CONSTRAINT FK_Table2_CustID1 FOREIGN KEY (CustID2) /****This command establis a relationships to the table1***/
         REFERENCES Table1(CustID1)
 );
 GO
