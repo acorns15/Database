b533d0153fff3749deff2ab5398624a9a8ad2ac9
Maycort Baculna <81124657+acorns15@users.noreply.github.com>
2021-05-23 23:41:11 +0800

    Update Table3_Data.sql

---
+++ Comprehensive_Project/Database/Table3/Table3_Data.sql
@@ -0,0 +1,41 @@
+INSERT INTO Customers (
+	Customer_NAME,
+	Address,
+	Contact_Number
+	)
+VAlUES (
+	'Darren Powers',
+	'Silay City',
+	09221238210
+	),
+	(
+	'Lena Hernandez',
+	'Bacolod City',
+	09325832485
+	),
+	(
+	'Karen Daniels',
+	'Bago City',
+	09743958392
+	),
+	(
+	'Paul Gonzalez',
+	'Sagay City',
+	09358240547
+	),
+	(
+	'John Steel',
+	'Bacolod City',
+	09732893984
+	),
+	(
+	'David Smith',
+	'Bacolod City',
+	09643829312
+	),
+	(
+	'Alan Haines',
+	'Bacolod City',
+	09478239504
+	)
+
