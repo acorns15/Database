3f61e45f25cbb1b3580ff758a7a36ea2cbcdfb86
Maycort Baculna <81124657+acorns15@users.noreply.github.com>
2021-05-24 00:20:08 +0800

    Update Table2_data.sql

---
+++ Comprehensive_Project/Database/Table2/Table2_data.sql
@@ -0,0 +1,44 @@
+INSERT INTO Products (
+	Product_Name,
+	Product_Categories,
+	Stock_Level,
+	Unit_Price
+)
+
+Values (
+	'Coke',
+	'Beverages',
+	1000,
+	15
+	),
+	(
+	'Martys Cracklin',
+	'Foods',
+	100,
+	8
+	),
+	(
+	'Nescafe',
+	'Beverages',
+	500,
+	2
+	),
+	(
+	'Pepsi',
+	'Beverages',
+	500,
+	14),
+	(
+	'Piattos',
+	'Foods',
+	100,
+	15),
+	('Boy Bawang',
+	'Foods',
+	50,
+	10),
+	('Chippy',
+	'Foods',
+	70,
+	5)
+
